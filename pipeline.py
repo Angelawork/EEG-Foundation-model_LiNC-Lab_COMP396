1f31ab13c2f7954633f9b3b0720895ab5769a584
Angelawork <Qingchen.hu@mail.mcgill.ca>
2024-09-17 10:41:39 -0400

    Rename pipeline with used model's name

--- pipeline.py
+++ pipeline.py
@@ -4,9 +4,14 @@ from moabb.evaluations import WithinSessionEvaluation, CrossSessionEvaluation, C
 from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
+import os
+os.environ["MNE_DATA"] = "./mne_data"
+os.environ["MOABB_RESULTS"] = "./mne_data"
 
 def run_pipeline(datasets, subjects, paradigm, model_pipeline, eval_scheme):
     """
+    Pipeline Model's name will be renamed according to the rename dict.
+
     Params:
     datasets: list of MOABB dataset
     subjects: list of int
@@ -25,27 +30,32 @@ def run_pipeline(datasets, subjects, paradigm, model_pipeline, eval_scheme):
              model_pipeline=make_pipeline(CSP(n_components=8), LDA()),
              eval_scheme="WithinSessionEvaluation")
     """
+    rename={"lineardiscriminantanalysis":"LDA"}
     eval_results = {}
 
     for ds in datasets:
       if not paradigm.is_valid(ds):
-          raise ValueError(f"{ds.code} not compatible with paradigm {type(paradigm).__name__}")
+        raise ValueError(f"{ds.code} not compatible with paradigm {type(paradigm).__name__}")
           
       if hasattr(ds, "get_data"):
-          ds.subject_list = subjects
+        ds.subject_list = subjects
       else:
-          raise ValueError(f"Provided dataset invalid: {ds}")
+        raise ValueError(f"Provided dataset invalid: {ds}")
 
       eval_scheme = WithinSessionEvaluation(paradigm=paradigm, datasets=[ds], overwrite=False)
       if isinstance(model_pipeline, Pipeline):
-          pipelines = {"custom_pipeline": model_pipeline}
+        pipe_name = "+".join([rename[step.__class__.__name__.lower()] if step.__class__.__name__.lower() in rename 
+                                else step.__class__.__name__ 
+                                for _, step in model_pipeline.steps])
+        pipelines = {pipe_name: model_pipeline}
       elif isinstance(model_pipeline, dict):
-          pipelines = model_pipeline
+        pipelines = model_pipeline
       else:
-          raise ValueError(f"Invalid pipeline: {model_pipeline}")
+        raise ValueError(f"Invalid pipeline: {model_pipeline}")
       
       print(f"Running the pipeline with subjects={subjects} on dataset={ds.code} using paradigm: {paradigm}")
       result = eval_scheme.process(pipelines)
+      result["pipeline"] = pipe_name
       eval_results[ds.code] = result
 
     return eval_results
@@ -63,8 +73,27 @@ def process_results(results):
     """
     df = pd.concat(results.values(), ignore_index=True)
     df=df.groupby(["pipeline", "dataset"])["score"].agg([np.mean, np.std])
-    df["scores"] = df.apply(lambda x: f"{x["mean"]:.2f}±{x["std"]:.2f}", axis=1)
+    df["scores"] = df.apply(lambda x: f'{x["mean"]:.2f}±{x["std"]:.2f}', axis=1)
     df = df.drop(columns=["mean", "std"]).unstack()
     df.columns = df.columns.droplevel()
     df=df.reset_index()
     return df
+
+if __name__ == "__main__":
+    from mne.decoding import CSP
+    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+    from sklearn.pipeline import make_pipeline
+    import warnings
+    import mne
+    import moabb
+    from moabb.datasets import BNCI2014_001, Zhou2016
+    from moabb.evaluations import WithinSessionEvaluation
+    from moabb.paradigms import LeftRightImagery
+    moabb.set_log_level("info")
+    mne.set_log_level("CRITICAL")
+    warnings.filterwarnings("ignore")
+
+    results=run_pipeline(datasets=[Zhou2016(), BNCI2014_001()], subjects=[1, 3] , paradigm=LeftRightImagery(),
+                model_pipeline=make_pipeline(CSP(n_components=8), LDA()),
+                eval_scheme="WithinSessionEvaluation")
+    print(results)
