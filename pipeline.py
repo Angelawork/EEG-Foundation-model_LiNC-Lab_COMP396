11d308e745411466a5e2294dc97aedc33bea9c1c
Angelawork <Qingchen.hu@mail.mcgill.ca>
2024-09-21 18:25:48 -0400

    run pipeline over customized ds

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
-from moabb.paradigms import MotorImagery
-from moabb.datasets import utils
+import utils
+import dataset_setup
+
 from moabb.evaluations import WithinSessionEvaluation, CrossSessionEvaluation, CrossSubjectEvaluation
 from sklearn.pipeline import Pipeline
 import pandas as pd
@@ -8,7 +9,7 @@ import os
 os.environ["MNE_DATA"] = "./mne_data"
 os.environ["MOABB_RESULTS"] = "./mne_data"
 
-def run_pipeline(datasets, subjects, paradigm, model_pipeline, eval_scheme):
+def run_pipeline(datasets, paradigm, model_pipeline, eval_scheme, random_state=24):
     """
     Pipeline Model's name will be renamed according to the rename dict.
 
@@ -20,29 +21,30 @@ def run_pipeline(datasets, subjects, paradigm, model_pipeline, eval_scheme):
     model_pipeline: Pipeline or dict
         a pre-built sklearn pipeline or a custom one as dict
     eval_scheme: moabb.evaluations object's name str
+    random_state: int
+        For CV result's reproducibility
+        If not None, can guarantee same seed for shuffling examples
 
     Returns:
     eval_results: dict
         results for each dataset, idx by dataset code
-
-    usage:
-    results=run_pipeline(datasets=[Zhou2016(), BNCI2014_001()], subjects=[1, 3] , paradigm=LeftRightImagery(),
-             model_pipeline=make_pipeline(CSP(n_components=8), LDA()),
-             eval_scheme="WithinSessionEvaluation")
     """
     rename={"lineardiscriminantanalysis":"LDA"}
     eval_results = {}
 
     for ds in datasets:
-        if not paradigm.is_valid(ds):
-            raise ValueError(f"{ds.code} not compatible with paradigm {type(paradigm).__name__}")
-          
-        if hasattr(ds, "get_data"):
-            ds.subject_list = subjects
+        if eval_scheme == "WithinSessionEvaluation":
+            evaluation = WithinSessionEvaluation(paradigm=paradigm, random_state=random_state,
+                                                  datasets=[ds], overwrite=True)
+        elif eval_scheme =="CrossSessionEvaluation":
+            evaluation = CrossSessionEvaluation(paradigm=paradigm, random_state=random_state,
+                                                 datasets=[ds], overwrite=True)
+        elif eval_scheme =="CrossSubjectEvaluation":
+            evaluation=CrossSubjectEvaluation(paradigm=paradigm, random_state=random_state,
+                                               datasets=[ds], overwrite=True)
         else:
-            raise ValueError(f"Provided dataset invalid: {ds}")
+            raise ValueError(f"Invalid eval_scheme: {eval_scheme}")
 
-        eval_scheme = WithinSessionEvaluation(paradigm=paradigm, datasets=[ds], overwrite=False)
         if isinstance(model_pipeline, Pipeline):
             pipe_name = "+".join([rename[step.__class__.__name__.lower()] if step.__class__.__name__.lower() in rename 
                                     else step.__class__.__name__ 
@@ -53,32 +55,47 @@ def run_pipeline(datasets, subjects, paradigm, model_pipeline, eval_scheme):
         else:
             raise ValueError(f"Invalid pipeline: {model_pipeline}")
         
-        print(f"Running the pipeline with subjects={subjects} on dataset={ds.code} using paradigm: {paradigm}")
-        result = eval_scheme.process(pipelines)
-        result["pipeline"] = pipe_name
-        result = result[result["subject"].astype(int).isin(subjects)]
-        eval_results[ds.code] = result
+        print(f"Running the pipeline on dataset={ds.code} using paradigm: {paradigm}")
+        result = evaluation.process(pipelines)
 
+        original_subject_ids = [subj[1] for subj in ds.subjects_list]
+        if isinstance(model_pipeline, Pipeline):
+          result["pipeline"] = pipe_name
+          result["subject"] =original_subject_ids[:len(result)]
+        else:
+          for pipe_name in pipelines:
+              if pipe_name in result.columns:
+                  result["subject"] = original_subject_ids[:len(result[pipe_name])]
+        eval_results[ds.code] = result
+        
     return eval_results
 
-def process_results(results):
-    """
-    Params:
-    results: dict of dataframes
-    returned by the run_pipeline() function, key = dataset name with value = corresponding results
+def read_setup(file_path):
+    subject_sessions=[]
+    with open(file_path, "r") as file:
+        for line in file:
+            subject_sessions+=[l.strip() for l in line.split()]
+    return subject_sessions
 
-    Returns:
-    df: dataframe
-        In the same format of MOABB benchmark with scores = mean ± std
+""" Usage example
+subject_sessions=read_setup("./benchmark1_subjects.txt")
+subject_sessions_setup = extract_subject_sessions(subject_sessions)
 
-    """
-    df = pd.concat(results.values(), ignore_index=True)
-    df=df.groupby(["pipeline", "dataset"])["score"].agg([np.mean, np.std])
-    df["scores"] = df.apply(lambda x: f'{x["mean"]:.2f}±{x["std"]:.2f}', axis=1)
-    df = df.drop(columns=["mean", "std"]).unstack()
-    df.columns = df.columns.droplevel()
-    df=df.reset_index()
-    return df
+paradigm=MotorImagery(n_classes=2, events=["left_hand", "right_hand"])
+test_ds_setup={"BNCI2014-001":subject_sessions_setup["BNCI2014-001"][:3],
+      "PhysionetMotorImagery":subject_sessions_setup["PhysionetMotorImagery"][:3]}
+filtered_setup=filter_data(paradigm,test_ds_setup)
+
+filtered_ds_cls=[]
+for name, setup in filtered_setup.items():
+  filtered_ds_cls.append(create_custom_ds(name,setup))
+example_pipeline = make_pipeline(CSP(), LDA())
+
+result=run_pipeline(datasets=filtered_ds_cls, paradigm=paradigm,
+                model_pipeline=example_pipeline, eval_scheme="WithinSessionEvaluation")
+print(process_results(result))
+"""
+
 
 if __name__ == "__main__":
     from mne.decoding import CSP
@@ -87,15 +104,8 @@ if __name__ == "__main__":
     import warnings
     import mne
     import moabb
-    from moabb.datasets import BNCI2014_001, Zhou2016
     from moabb.evaluations import WithinSessionEvaluation
-    from moabb.paradigms import LeftRightImagery
+    from moabb.paradigms import MotorImagery
     moabb.set_log_level("info")
     mne.set_log_level("CRITICAL")
     warnings.filterwarnings("ignore")
-
-    results=run_pipeline(datasets=[Zhou2016(), BNCI2014_001()], subjects=[1, 3] , paradigm=LeftRightImagery(),
-             model_pipeline=make_pipeline(CSP(n_components=8), LDA()),
-             eval_scheme="WithinSessionEvaluation")
-    print(results)
-    print(process_results(results))
